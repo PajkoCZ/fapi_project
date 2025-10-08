02b3fa26e138d87628cfbf3d5a7566410bc7625a
PM-Production <it.prislusenstvi@gmail.com>
2025-10-08 09:55:27 +0200

    Finální úpravy

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router'
+import OrderPage from './pages/OrderPage/OrderPage'
+import ThankYou from './pages/ThankYou/ThankYou'
 
 export default function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <BrowserRouter>
+      <div className='app-container'>
+        <header>
+          <h1>FAPI - Testovací úloha - Objednávkový systém</h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<OrderPage />} />
+            <Route path="/thankyou" element={<ThankYou />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 
-// https://vite.dev/config/
-export default defineConfig({
+const config: UserConfig = {
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',  // 👈 musí být jsdom
+    globals: true,          // expect() bez importu
+    setupFiles: './src/tests/setupTests.ts',
+    // exclude: [...configDefaults.exclude], // volitelné
+  },
+}
+
+export default defineConfig(config)
